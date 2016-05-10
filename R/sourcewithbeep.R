b1ffc72b5a60025ebf1df78a98483fbd2c431a7d
crazybilly@gmail.com <crazybilly@gmail.com>
2016-05-10 16:58:16 -0500

    Fix crashing.

--- R/sourcewithbeep.R
+++ R/sourcewithbeep.R
@@ -1,26 +1,23 @@
+#' Source with Beep
+#'
+#' @description source an R script and beep when you're done
+#'
+#' @param filename the file location that you wish to source. If the current file is already saved, it will use the filename. If not, or if you're on the console, it'll ask you for a filename.
+#' @param beep an integer, passed to beepr::beep to set which beep should be used. Defaults to 1.
+#'
+#' @return
+#' @export
+#'
+sourcewithbeep  <- function( filename = "", beep=1 ) {
 
+   if( rstudioapi::getActiveDocumentContext()$path != "" ) {
+          filename  <- rstudioapi::getActiveDocumentContext()$path
+   } else {
+      filename  <- readline("unquoted file location: ")
+   }
 
-sourcewithbeep  <- function( ) {
-
-   currentfile  <- rstudioapi::getActiveDocumentContext()
-
-   currentfilepath  <- currentfile$path
-
-   if( currentfilepath == '') {
-
-      write.table(currentfile$contents,file = '~/.active-rstudio-document2', row.names = F, col.names = F, quote = F)
-      source('~/.active-rstudio-document2', echo=T)
-      file.remove('~/.active-rstudio-document2')
-
-    } else {
-
-       source(currentfilepath, echo = T)
-
-    }
-
-   beepr::beep()
+   source(filename, echo = T)
+   beepr::beep( beep )
 
 }
-
-
 
